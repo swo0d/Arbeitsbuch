d2749e5cf25118eff54da93514682b0e2c38ab74
swod <swod@arcor.de>
2023-05-11 21:36:16 +0200

    adapter_Listener

--- app/src/main/java/com/example/arbeitsbuch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/arbeitsbuch/presentation/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.arbeitsbuch.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.RecoverySystem
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.arbeitsbuch.R
 import com.example.arbeitsbuch.domain.ObjectItem
@@ -14,21 +16,69 @@ import com.example.arbeitsbuch.domain.ObjectItem
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var adapter: ObjectListAdapter
+    private lateinit var objectListAdapter: ObjectListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupRecyclerView()
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.objectList.observe(this) {
-            adapter.objectList = it
+            objectListAdapter.objectList = it
         }
     }
 
     private fun setupRecyclerView() {
         val rvObjectList = findViewById<RecyclerView>(R.id.rv_object_list)
-        adapter = ObjectListAdapter()
-        rvObjectList.adapter = adapter
+        with(rvObjectList) {
+            objectListAdapter = ObjectListAdapter()
+            adapter = objectListAdapter
+            recycledViewPool.setMaxRecycledViews(
+                ObjectListAdapter.VIEW_TYPE_ENABLED,
+                ObjectListAdapter.MAX_POOL_SIZE
+            )
+            recycledViewPool.setMaxRecycledViews(
+                ObjectListAdapter.VIEW_TYPE_DISABLED,
+                ObjectListAdapter.MAX_POOL_SIZE
+            )
+        }
+        setupLongClickListener()
+        setupClickListener()
+        setupSwipeListener(rvObjectList)
     }
 
+    private fun setupSwipeListener(rvObjectList: RecyclerView) {
+        val callback = object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val item = objectListAdapter.objectList[viewHolder.adapterPosition]
+                viewModel.deleteObjectItem(item)
+            }
+
+        }
+        val itemTouchHelper = ItemTouchHelper(callback)
+        itemTouchHelper.attachToRecyclerView(rvObjectList)
+    }
+
+    private fun setupClickListener() {
+        objectListAdapter.onObjectItemClickListener = {
+            Log.d("MainActivity", it.toString())
+        }
+    }
+
+    private fun setupLongClickListener() {
+        objectListAdapter.onObjectItemLongClickListener = {
+            viewModel.changeEnableState(it)
+        }
+    }
 }
--- app/src/main/java/com/example/arbeitsbuch/presentation/ObjectListAdapter.kt
+++ app/src/main/java/com/example/arbeitsbuch/presentation/ObjectListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.arbeitsbuch.presentation
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,17 +11,25 @@ import com.example.arbeitsbuch.R
 import com.example.arbeitsbuch.domain.ObjectItem
 
 class ObjectListAdapter : RecyclerView.Adapter<ObjectListAdapter.ObjectItemViewHolder>() {
-
+    var count = 0
     var objectList = listOf<ObjectItem>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
+        var onObjectItemLongClickListener: ((ObjectItem) -> Unit)? = null
+        var onObjectItemClickListener: ((ObjectItem) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ObjectItemViewHolder {
+       // Log.d("ObjectListAdapter","onCreateViewHolder, count: ${++count}")
+        val layout = when (viewType) {
+            VIEW_TYPE_ENABLED -> R.layout.item_object_list_enabled
+            VIEW_TYPE_DISABLED -> R.layout.item_object_list_disabled
+            else -> throw java.lang.RuntimeException("Unknown view type: $viewType")
+        }
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_object_list_disabled, parent, false)
+            .inflate(layout, parent, false)
         return ObjectItemViewHolder(view)
     }
 
@@ -29,21 +38,18 @@ class ObjectListAdapter : RecyclerView.Adapter<ObjectListAdapter.ObjectItemViewH
     }
 
     override fun onBindViewHolder(viewHolder: ObjectItemViewHolder, position: Int) {
+        Log.d("ObjectListAdapter", "onBindViewHolder, count: ${++count}" )
         val objectItem = objectList[position]
-        val status = if (objectItem.enabled) {
-            "Active"
-        } else {
-            "Not active"
-        }
-
         viewHolder.view.setOnLongClickListener {
+            onObjectItemLongClickListener?.invoke(objectItem)
             true
         }
-        if (objectItem.enabled) {
-            viewHolder.tvName.text = "${objectItem.name} $status"
-            viewHolder.tvDate.text = objectItem.date.toString()
-            viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context, android.R.color.holo_red_dark))
+        viewHolder.view.setOnClickListener {
+            onObjectItemClickListener?.invoke(objectItem)
+            true
         }
+        viewHolder.tvName.text = objectItem.name
+        viewHolder.tvDate.text = objectItem.date.toString()
     }
 
     override fun onViewRecycled(viewHolder: ObjectItemViewHolder) {
@@ -53,8 +59,24 @@ class ObjectListAdapter : RecyclerView.Adapter<ObjectListAdapter.ObjectItemViewH
             .getColor(viewHolder.view.context, android.R.color.white))
     }
 
+    override fun getItemViewType(position: Int): Int {
+        val item = objectList[position]
+        return  if (item.enabled) {
+            VIEW_TYPE_ENABLED
+        } else {
+            VIEW_TYPE_DISABLED
+        }
+    }
+
+
     class ObjectItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val tvName = view.findViewById<TextView>(R.id.list_item_object_name)
         val tvDate = view.findViewById<TextView>(R.id.list_item_object_date)
+    }
+
+    companion object {
+        const val VIEW_TYPE_ENABLED = 100
+        const val VIEW_TYPE_DISABLED = 101
+        const val MAX_POOL_SIZE = 10
     }
 }
