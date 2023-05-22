1b9ca4e2e9feab62dd22a5970877bbf3afeb88e6
swod <swod@arcor.de>
2023-05-22 22:52:48 +0200

    fragment_onhe_logic

--- app/src/main/java/com/example/arbeitsbuch/presentation/ObjectItemActivity.kt
+++ app/src/main/java/com/example/arbeitsbuch/presentation/ObjectItemActivity.kt
@@ -18,127 +18,135 @@ import com.example.arbeitsbuch.R
 import com.example.arbeitsbuch.domain.ObjectItem
 import com.google.android.material.textfield.TextInputLayout
 
-class ObjectItemActivity: AppCompatActivity() {
+class ObjectItemActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: ObjectItemViewModel
-
-    private lateinit var tvObjectName: TextView
-    private lateinit var etObjectName: EditText
-    private lateinit var buttonSave: Button
-
-    private var screenMode = MODE_UNKNOWN
-    private var objectItemId = ObjectItem.UNDEFINED_ID
-
+    //    private lateinit var viewModel: ObjectItemViewModel
+//
+//    private lateinit var tvObjectName: TextView
+//    private lateinit var etObjectName: EditText
+//    private lateinit var buttonSave: Button
+//
+//    private var screenMode = MODE_UNKNOWN
+//    private var objectItemId = ObjectItem.UNDEFINED_ID
+//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.view_camera_and_title)
-        parseIntent()
-        viewModel = ViewModelProvider(this)[ObjectItemViewModel::class.java]
-        initViews()
-        addTextChangeListener()
-        changeRightMode()
-        observeViewModel()
+        setContentView(R.layout.item_object)
+//        //setContentView(R.layout.item_object)
+//
+//        parseIntent()
+//        viewModel = ViewModelProvider(this)[ObjectItemViewModel::class.java]
+//        initViews()
+//        addTextChangeListener()
+//        changeRightMode()
+//        observeViewModel()
     }
+//
+//    private fun observeViewModel() {
+//        viewModel.errorInputName.observe(this) {
+//            val message = if (it) {
+//                getString(R.string.error_input_name)
+//            } else {
+//                null
+//            }
+//            etObjectName.error = message
+//        }
+//        viewModel.shouldCloseScreen.observe(this) {
+//            finish()
+//        }
+//    }
+//
+//    private fun changeRightMode() {
+//        when (screenMode) {
+//            MODE_EDIT -> launchEditMode()
+//            MODE_ADD -> launchAddMode()
+//        }
+//    }
+//
+//    private fun addTextChangeListener() {
+//        etObjectName.addTextChangedListener(object : TextWatcher {
+//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//
+//            }
+//
+//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                viewModel.resetErrorInputName()
+//            }
+//
+//            override fun afterTextChanged(p0: Editable?) {
+//
+//            }
+//        })
+//    }
+//
+//    private fun launchEditMode() {
+//        bSaveVisibility()
+//        viewModel.getObjectItem(objectItemId)
+//        viewModel.objectItem.observe(this) {
+//            etObjectName.setText(it.name)
+//        }
+//        buttonSave.setOnClickListener {
+//            viewModel.editObjectItem(etObjectName.text?.toString())
+//        }
+//    }
+//
+//    private fun launchAddMode() {
+//        bSaveVisibility()
+//        buttonSave.setOnClickListener {
+//            viewModel.addObjectItem(etObjectName.text?.toString())
+//
+//        }
+//    }
+//
+//    private fun bSaveVisibility() {
+//        buttonSave.visibility = View.VISIBLE
+//    }
+//
+//    private fun parseIntent() {
+//        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+//            throw RuntimeException("Param screen mode is absent")
+//        }
+//        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+//        if (mode != MODE_EDIT && mode != MODE_ADD) {
+//            throw java.lang.RuntimeException("Unknown screen mode $mode")
+//        }
+//        screenMode = mode
+//        if (screenMode == MODE_EDIT) {
+//            if (!intent.hasExtra(EXTRA_OBJECT_ITEM_ID)) {
+//                throw RuntimeException("Param object item id in absent")
+//            }
+//            objectItemId = intent.getIntExtra(EXTRA_OBJECT_ITEM_ID, ObjectItem.UNDEFINED_ID)
+//        }
+//    }
 
-    private fun observeViewModel() {
-        viewModel.errorInputName.observe(this){
-            val message = if (it) {
-               getString(R.string.error_input_name)
-            }else {
-                null
+//
+//    private fun initViews() {
+//        tvObjectName = findViewById(R.id.tv_object_name)
+//        etObjectName = findViewById(R.id.et_object_name)
+//        buttonSave = findViewById(R.id.b_save)
+//
+//    }
+
+        companion object {
+
+            private const val EXTRA_SCREEN_MODE = "extra_mode"
+            private const val EXTRA_OBJECT_ITEM_ID = " extra_object_item_id"
+            private const val MODE_EDIT = "mode_edit"
+            private const val MODE_ADD = "mode_add"
+            private const val MODE_UNKNOWN = ""
+
+
+            fun newIntentAddObject(context: Context): Intent {
+                val intent = Intent(context, ObjectItemActivity::class.java)
+                intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+                return intent
             }
-            etObjectName.error = message
-        }
-        viewModel.shouldCloseScreen.observe(this) {
-            finish()
-        }
-    }
-    private fun changeRightMode(){
-        when (screenMode) {
-            MODE_EDIT -> launchEditMode()
-            MODE_ADD -> launchAddMode()
-        }
-    }
-    private fun addTextChangeListener(){
-        etObjectName.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
+            fun newIntentEditObject(context: Context, objectItemId: Int): Intent {
+                val intent = Intent(context, ObjectItemActivity::class.java)
+                intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+                intent.putExtra(EXTRA_OBJECT_ITEM_ID, objectItemId)
+                return intent
             }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                viewModel.resetErrorInputName()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
-        })
-    }
-    private  fun launchEditMode(){
-        bSaveVisibility()
-        viewModel.getObjectItem(objectItemId)
-        viewModel.objectItem.observe(this) {
-            etObjectName.setText(it.name)
-        }
-        buttonSave.setOnClickListener {
-            viewModel.editObjectItem(etObjectName.text?.toString())
         }
     }
-    private  fun launchAddMode(){
-        bSaveVisibility()
-        buttonSave.setOnClickListener {
-            viewModel.addObjectItem(etObjectName.text?.toString())
-
-        }
-    }
-    private fun bSaveVisibility(){
-        buttonSave.visibility = View.VISIBLE
-    }
-    private fun parseIntent(){
-        if(!intent.hasExtra(EXTRA_SCREEN_MODE)){
-            throw RuntimeException("Param screen mode is absent")
-        }
-        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
-        if(mode != MODE_EDIT && mode != MODE_ADD) {
-            throw java.lang.RuntimeException("Unknown screen mode $mode")
-        }
-        screenMode = mode
-        if (screenMode == MODE_EDIT) {
-            if (!intent.hasExtra(EXTRA_OBJECT_ITEM_ID)) {
-                throw RuntimeException("Param object item id in absent")
-            }
-            objectItemId = intent.getIntExtra(EXTRA_OBJECT_ITEM_ID, ObjectItem.UNDEFINED_ID)
-        }
-    }
-
-    private fun initViews(){
-        tvObjectName = findViewById(R.id.tv_object_name)
-        etObjectName = findViewById(R.id.et_object_name)
-        buttonSave = findViewById(R.id.b_save)
-
-    }
-
-
-    companion object {
-
-        private const val EXTRA_SCREEN_MODE = "extra_mode"
-        private const val EXTRA_OBJECT_ITEM_ID = " extra_object_item_id"
-        private const val MODE_EDIT = "mode_edit"
-        private const val MODE_ADD = "mode_add"
-        private const val MODE_UNKNOWN = ""
-
-
-        fun newIntentAddObject(context: Context): Intent {
-            val intent = Intent(context,ObjectItemActivity::class.java)
-            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
-            return intent
-        }
-
-        fun newIntentEditObject(context: Context, objectItemId: Int): Intent {
-            val intent = Intent(context,ObjectItemActivity::class.java)
-            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
-            intent.putExtra(EXTRA_OBJECT_ITEM_ID, objectItemId)
-            return intent
-        }
-    }
-}
---
+++ app/src/main/java/com/example/arbeitsbuch/presentation/ObjectItemFragment.kt
@@ -0,0 +1,18 @@
+package com.example.arbeitsbuch.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.arbeitsbuch.R
+
+class ObjectItemFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_object_item, container, false)
+    }
+}
